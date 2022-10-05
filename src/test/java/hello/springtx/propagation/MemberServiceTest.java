b3a624ef3ef9a196a17bc7da7644c96561a92233
lee <anomalocaris3871@gmail.com>
2022-10-05 21:19:01 +0900

    add transaction propagation example

---
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired LogRepository logRepository;
+
+    @Test
+    public void outerTxOff_success() {
+        String username = "outerTxOff_success";
+
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+
+    }
+
+    @Test
+    public void outerTxOff_fail() {
+        String username = "logException_outerTxOff_fail";
+
+        assertThatThrownBy(()-> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+
+    }
+
+    @Test
+    public void singleTx() {
+        String username = "outerTxOn_success";
+
+        memberService.joinV1(username);
+
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+
+    }
+}
